8f3f80257fa87b59fc50b5e680015395c62d1d3b
subin <ksb4150@mapmind.kr>
2025-09-30 18:31:47 +0900

    project init

---
+++ src/test/java/shopping/ecco/com/EccoApplicationTests.java
@@ -0,0 +1,13 @@
+package shopping.ecco.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
